6b57075b027394a51663b47a085ce3288b5a9ff9
Harry-Bro <likeharrybro@gmail.com>
2021-05-15 15:42:35 +0900

    feat(IS-28): Add Contract

---
+++ src/main/java/com/dogfoot/insurancesystemserver/domain/contract/domain/Contract.java
@@ -0,0 +1,42 @@
+package com.dogfoot.insurancesystemserver.domain.contract.domain;
+
+import com.dogfoot.insurancesystemserver.domain.accident.domain.Accident;
+import com.dogfoot.insurancesystemserver.domain.compensation.domain.Compensation;
+import com.dogfoot.insurancesystemserver.domain.insurance.domain.Insurance;
+import com.dogfoot.insurancesystemserver.domain.user.domain.User;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+public class Contract {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @OneToOne
+    private Insurance insurance;
+
+    @OneToMany(mappedBy = "contract")
+    private List<Accident> accidentList;
+
+    @OneToMany(mappedBy = "contract")
+    private List<Compensation> compensationList;
+
+    private LocalDate expirationDate;
+
+    @CreationTimestamp
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp updatedDate;
+
+}
