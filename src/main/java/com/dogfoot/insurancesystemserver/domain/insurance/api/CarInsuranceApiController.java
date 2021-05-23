956dc7ae120edb3d4ec3199037dc29c3a48f32ca
Harry-Bro <likeharrybro@gmail.com>
2021-05-23 16:53:23 +0900

    feat(IS-159, IS-161, IS-163): Add insurance api controller

--- src/main/java/com/dogfoot/insurancesystemserver/domain/insurance/api/CarInsuranceApiController.java
+++ src/main/java/com/dogfoot/insurancesystemserver/domain/insurance/api/CarInsuranceApiController.java
@@ -1,37 +1,19 @@
 package com.dogfoot.insurancesystemserver.domain.insurance.api;
 
+import com.dogfoot.insurancesystemserver.domain.insurance.domain.CarInsurance;
 import com.dogfoot.insurancesystemserver.domain.insurance.dto.CarInsuranceDetailResponse;
-import com.dogfoot.insurancesystemserver.domain.insurance.dto.InsuranceResponse;
-import com.dogfoot.insurancesystemserver.domain.insurance.service.CarInsuranceService;
-import com.dogfoot.insurancesystemserver.global.dto.Pagination;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("api/v1/user/car")
 @RestController
-public class CarInsuranceApiController implements InsuranceApiController<CarInsuranceDetailResponse>{
-
-    private final CarInsuranceService insuranceService;
-
-    @Override
-    public ResponseEntity<Pagination<List<InsuranceResponse>>> listByAvailableSale(Pageable pageable) {
-        return ResponseEntity.ok(this.insuranceService.listByAvailableSale(pageable));
-    }
+public class CarInsuranceApiController extends InsuranceApiControllerImpl<CarInsuranceDetailResponse, CarInsurance> {
 
 //    @GetMapping("user/car/insurance/unavailable/list")
 //    public ResponseEntity<Pagination<List<CarInsuranceResponse>>> listByUnAvailableSale(@PageableDefault Pageable pageable) {
 //            return ResponseEntity.ok(this.carInsuranceService.listByUnAvailableSale(pageable));
 //    }
-
-    @Override
-    public ResponseEntity<CarInsuranceDetailResponse> detail(Long id) {
-        return ResponseEntity.ok(this.insuranceService.read(id));
-    }
 
 }
--- src/main/java/com/dogfoot/insurancesystemserver/domain/insurance/api/DriverInsuranceApiController.java
+++ src/main/java/com/dogfoot/insurancesystemserver/domain/insurance/api/DriverInsuranceApiController.java
@@ -1,31 +1,13 @@
 package com.dogfoot.insurancesystemserver.domain.insurance.api;
 
+import com.dogfoot.insurancesystemserver.domain.insurance.domain.DriverInsurance;
 import com.dogfoot.insurancesystemserver.domain.insurance.dto.DriverInsuranceDetailResponse;
-import com.dogfoot.insurancesystemserver.domain.insurance.dto.InsuranceResponse;
-import com.dogfoot.insurancesystemserver.domain.insurance.service.DriverInsuranceService;
-import com.dogfoot.insurancesystemserver.global.dto.Pagination;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("api/v1/user/driver")
 @RestController
-public class DriverInsuranceApiController implements InsuranceApiController<DriverInsuranceDetailResponse> {
-
-    private final DriverInsuranceService insuranceService;
-
-    @Override
-    public ResponseEntity<Pagination<List<InsuranceResponse>>> listByAvailableSale(Pageable pageable) {
-        return ResponseEntity.ok(this.insuranceService.listByAvailableSale(pageable));
-    }
-
-    @Override
-    public ResponseEntity<DriverInsuranceDetailResponse> detail(Long id) {
-        return ResponseEntity.ok(this.insuranceService.read(id));
-    }
+public class DriverInsuranceApiController extends InsuranceApiControllerImpl<DriverInsuranceDetailResponse, DriverInsurance> {
 }
